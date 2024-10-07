1bc12458da99de1ed998f22cdd77c39ba65dc79b
premsai-noolu <premsainoolu01@gmail.com>
2024-10-07 11:02:56 +0530

    created Routes and handlers

--- src/app.js
+++ src/app.js
@@ -7,16 +7,37 @@ app.use("/greet", (req, res) => {
 });
 
 app.use("/user", (req, res) => {
-  res.send("name is prem sai");
+  res.send("Hahahahaha");
+});
+
+app.get("/user", (req, res) => {
+  res.send({ firstname: "prem", lastname: "noolu" });
+});
+
+app.post("/user", (req, res) => {
+  //Data saved to DB
+  res.send("Data successfully saved to DB");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted from DB");
+});
+
+app.use("/hello/123", (req, res) => {
+  res.send("hello hello123");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello hello hello");
 });
 
 app.use("/test", (req, res) => {
   res.send("hello world test");
 });
 
-app.use((req, res) => {
-  res.send("hello world 1");
-});
+// app.use((req, res) => {
+//   res.send("hello world 1");
+// });
 
 app.listen(9999, () => {
   console.log("server is running on 9999");
