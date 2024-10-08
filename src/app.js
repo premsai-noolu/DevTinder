5178a8de40c01365740821d63019bd8c9f176f71
premsai-noolu <premsainoolu01@gmail.com>
2024-10-08 11:51:09 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,38 +2,60 @@ const express = require("express");
 
 const app = express();
 
-app.use("/greet", (req, res) => {
-  res.send("Namaste Prem");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("This is first response");
+    // res.send("Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("This is second response");
+    // res.send("2nd response!!");
+    next();
+  },
+  (req, res) => {
+    console.log("This is third response");
+    res.send("3rd response!!");
+  },
+  (req, res) => {
+    console.log("This is fourth response");
+    res.send("4th response!!");
+  }
+);
 
-app.use("/user", (req, res) => {
-  res.send("Hahahahaha");
-});
+// app.use("/greet", (req, res) => {
+//   res.send("Namaste Prem");
+// });
 
-app.get("/user", (req, res) => {
-  res.send({ firstname: "prem", lastname: "noolu" });
-});
+// app.use("/user", (req, res) => {
+//   res.send("Hahahahaha");
+// });
 
-app.post("/user", (req, res) => {
-  //Data saved to DB
-  res.send("Data successfully saved to DB");
-});
+// app.get("/user", (req, res) => {
+//   res.send({ firstname: "prem", lastname: "noolu" });
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("Data deleted from DB");
-});
+// app.post("/user", (req, res) => {
+//   //Data saved to DB
+//   res.send("Data successfully saved to DB");
+// });
 
-app.use("/hello/123", (req, res) => {
-  res.send("hello hello123");
-});
+// app.delete("/user", (req, res) => {
+//   res.send("Data deleted from DB");
+// });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello");
-});
+// app.use("/hello/123", (req, res) => {
+//   res.send("hello hello123");
+// });
 
-app.use("/test", (req, res) => {
-  res.send("hello world test");
-});
+// app.use("/hello", (req, res) => {
+//   res.send("Hello hello hello");
+// });
+
+// app.use("/test", (req, res) => {
+//   res.send("hello world test");
+// });
 
 // app.use((req, res) => {
 //   res.send("hello world 1");
