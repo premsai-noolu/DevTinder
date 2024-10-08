968fc9b39a1ad09bb09037466a7e32fd860c288d
premsai-noolu <premsainoolu01@gmail.com>
2024-10-08 12:27:09 +0530

    "multiple route handlers 1"

--- src/app.js
+++ src/app.js
@@ -2,27 +2,51 @@ const express = require("express");
 
 const app = express();
 
+app.use("/", (req, res, next) => {
+  console.log("/response");
+  // res.send("/response");
+  next();
+});
+
+app.use("/greet", (req, res) => {
+  console.log("greet function");
+  res.send("greet response");
+});
+
 app.use(
   "/user",
   (req, res, next) => {
-    console.log("This is first response");
-    // res.send("Response!!");
+    console.log("This is first user response");
+    // res.send(" first user response");
     next();
   },
   (req, res, next) => {
-    console.log("This is second response");
-    // res.send("2nd response!!");
-    next();
-  },
-  (req, res) => {
-    console.log("This is third response");
-    res.send("3rd response!!");
-  },
-  (req, res) => {
-    console.log("This is fourth response");
-    res.send("4th response!!");
+    console.log("This is second user response");
+    res.send("second user response");
   }
 );
+
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("This is first response");
+//     // res.send("Response!!");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("This is second response");
+//     // res.send("2nd response!!");
+//     next();
+//   },
+//   (req, res) => {
+//     console.log("This is third response");
+//     res.send("3rd response!!");
+//   },
+//   (req, res) => {
+//     console.log("This is fourth response");
+//     res.send("4th response!!");
+//   }
+// );
 
 // app.use("/greet", (req, res) => {
 //   res.send("Namaste Prem");
