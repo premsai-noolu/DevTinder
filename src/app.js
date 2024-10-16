1db34b48a40a736544434791c0e60f20d18f76ac
premsai-noolu <premsainoolu01@gmail.com>
2024-10-16 23:49:22 +0530

    connecting to DB

--- src/app.js
+++ src/app.js
@@ -1,27 +1,15 @@
 const express = require("express");
 
 const app = express();
+const { connectDB } = require("./config/database");
 
-app.get("/getuserdata", (req, res) => {
-  try {
-    throw new err("thos is a error");
-    res.send("helloooooooooooooooooooooo");
-  } catch (err) {
-    res.status(500).send("something went wrong");
-  }
-});
-
-app.get("/getdata", (req, res) => {
-  throw new err("thos is a error");
-  res.send("helloooooooooooooooooooooo");
-});
-
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("something went wrong hoo");
-  }
-});
-
-app.listen(9999, () => {
-  console.log("server is running on 9999");
-});
+connectDB()
+  .then(() => {
+    console.log("databse connection established");
+    app.listen(9999, () => {
+      console.log("server is running on 9999");
+    });
+  })
+  .catch((err) => {
+    console.log("database connection not established");
+  });
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://Premsai:RSacGWUe3tsaT0Xe@namastenode.rdsee.mongodb.net/"
+  );
+};
+
+module.exports = { connectDB };
