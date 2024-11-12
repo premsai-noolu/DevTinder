5f3d7e5f14c150ad15b9dfd200b730e30a34c5a9
premsai-noolu <premsainoolu01@gmail.com>
2024-11-12 19:11:41 +0530

    logout expiring cookie

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const { connectDB } = require("./config/database");
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
