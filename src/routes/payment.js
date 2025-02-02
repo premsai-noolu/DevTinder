1badbb7d043e9e5f9f2c94a74195473f07d35e53
premsai-noolu <premsainoolu01@gmail.com>
2025-02-02 20:29:22 +0530

    Working on payment route

---
+++ src/routes/payment.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const paymentRouter = express.Router();
+
+paymentRouter.post("/payment/create", userAuth, async (req, res) => {});
+
+module.exports = paymentRouter;
